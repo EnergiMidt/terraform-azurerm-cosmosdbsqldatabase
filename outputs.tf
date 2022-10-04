4840cb2c117ce5817bd3e7da309b8d113fae9d99
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-04 21:27:10 +0200

    feature: upgrade terraform and terraform provider version

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,3 @@ output "cosmosdb_sql_database" {
   description = "The Azure CosmosDB SQL Database resource."
   value       = local.cosmosdb_sql_database
 }
-output "cosmosdb_sql_database_name" {
-  description = "The name of Azure CosmosDB SQL Database resource."
-  value       = local.name
-}
